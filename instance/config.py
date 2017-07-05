4b994532dc730dcdde1f6426eb2822e4939f86b9
Nicolas Tallineau <nicolas.tallineau@ubisoft.com>
2017-07-05 14:39:48 -0400

    Initial commit

---
+++ instance/config.py
@@ -0,0 +1,28 @@
+# /instance/config.py
+
+import os
+
+class Config(object):
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgres://ajbtuzbq:GBzd5nVae0xkzXa2tcFSB0mV6gIAuB-D@stampy.db.elephantsql.com:5432/ajbtuzbq'
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+}
